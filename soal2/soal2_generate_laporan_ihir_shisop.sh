e292e9cbdd5f4d9eb581e1035e46ef490b9eae8f
Fuad Elhasan Irfani <73324192+feilvan@users.noreply.github.com>
2021-04-04 18:58:49 +0700

    Update soal2_generate_laporan_ihir_shisop.sh

--- soal2/soal2_generate_laporan_ihir_shisop.sh
+++ soal2/soal2_generate_laporan_ihir_shisop.sh
@@ -1,74 +1,79 @@
 #!/bin/bash
 export LC_ALL=C
-awk'
+awk '
 BEGIN{FS="\t"}
 {
-profit=$21;
-costprice=($18-$21);
-profitpercentage=(profit/costprice)*100
-#untuk mencari profit percentage dan Row ID terbesar
-if(maks<=profitpercentage){
-maks=profitpercentage;
-RowID=$1s}
+  profit=$21
+  sales=$18
+  profitpercentage=profit/(sales-profit)*100
+  #untuk mencari profit percentage dan Row ID terbesar
+  if(maks<=profitpercentage){
+    maks=profitpercentage
+    RowID=$1
+  }
 }
 END{
-printf("Transaksi terakhir dengan profit percentage terbesar yaitu %d dengan persentase %d%.",RowID,profitpercentage)
-}'/home/Downloads/Laporan-TokoShiSop.tsv >> hasil.txt
+    printf("Transaksi terakhir dengan profit persentase terbesar yaitu %d dengan persentase %d%.",RowID,max)
+} ' /home/sabrina/Documents/sisop/Laporan-TokoShiSop.tsv >> hasil.txt
 
 #2b
-awk'
+export LC_ALL=C
+awk '
 BEGIN{FS="\t"}
 {
-orderID=$2
-City=$10
-customername=$7
-
-if(orderID~"2017" && city=="Albuquerque"){
-arrname[customername]++}
+  orderID=$2
+  city=$10
+  customername=$7
+  if(orderID~"2017" && city=="Albuquerque"){
+    arrname[customername]++
+  }
 }
 END{
-print "\nDaftar nama customer di Albuquerque pada tahun 2017 antara lain:\n"
-{
-for(customername in arrname){
-{print customername}
-}/home/sabrina/Downloads/Laporan-TokoShiSop.tsv >> hasil.txt
+  print "Daftar nama customer di Albuquerque pada tahun 2017 antara lain:"
+  for(customername in arrname)
+  {print customername}
+}' /home/sabrina/Documents/sisop/Laporan-TokoShiSop.tsv >> hasil.txt
 
 
 #2c
 export LC_ALL=C
-awk'
+awk '
 BEGIN{FS="\t"}
 {
-#mengecualikan row paling atas
-if(NR!=1){
-arrsegment[$8]}
+  segment=$8
+  if(NR>1){
+    arrsegment[segment]++
+  }
 }
 END{
-minsales=10000
-for(segment in arrsegment){
-if(minsales>arrsegment[segment]){
-minsales=arrsegment[segment]
-sum=segment;
-}
-}
-printf("\nTipe segment customer yang penjualannya paling sedikit adalah %s dengan %.1f transaksi.\n",sum,minsales)
-}/home/sabrina/Downloads/Laporam-TokoShiShop.tsv >> hasil.txt
+  minsales=5000
+  for(segment in arrsegment){
+    if(minsales > arrsegment[segment]){
+      minsales=arrsegment[segment]
+      namasegment=segment;
+    }
+  }
+  printf("\nTipe segmen customer yang penjualannya paling sedikit adalah %s dengan %.1f transaksi.\n",namasegment,minsales)
+}' /home/sabrina/Documents/sisop/Laporam-TokoShiSop.tsv >> hasil.txt
 
 #2d
 export LC_ALL=C
-awk'
+awk '
 BEGIN{FS="\t"}
 {
-if(NR!=1){
-arrregion[$13]=+$21}
+  region=$13
+  profit=$21
+  if(NR>1){
+    arrregion[region]=arrregion[region]+profit;
+  }
 }
 END{
-minprofit=100000
-for(region in arrregion){
-if(minprofit>arrregion[region]){
-minprofit=arrregion[region]
-wilayah=region
-}
-}
-printf("\nWilayah bagian (region) yang memiliki total keuntungan (profit) yang paling sedikit adalah %s dengan total keuntungan %.1f",wilayah,minprofit)
-}/home/sabrina/Downloads/TokoShiSop.tsv >> hasil.txt
+  totalprofit=100000
+  for(region in arrregion){
+    if (totalprofit > arrregion[region]){
+      totalprofit = arrregion[region]
+      namaregion = region
+    }
+  }
+  printf("Wilayah bagian (region) yang memiliki total keuntungan (profit) yang paling sedikit adalah %s dengan total keuntungan %.1f",namaregion,totalprofit)
+}' /home/sabrina/Documents/sisop/TokoShiSop.tsv >> hasil.txt
